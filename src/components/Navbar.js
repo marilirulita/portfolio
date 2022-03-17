2642fdaea8baba285e3c31b83fffb0162c21ff29
marysol <marilirulita@gmail.com>
2022-03-17 02:16:35 -0700

    install linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,20 @@
 import styles from '../styles/Navbar.module.css';
 
-const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <div className={styles['nav-left']}>
-        <a href="/">Icon logo</a>
-        <a href="/">Github</a>
-        <a href="/">Linkedin</a>
-        <a href="/">Twitter</a>
-      </div>
-      <div className={styles['nav-right']}>
+const Navbar = () => (
+  <nav className={styles.navbar}>
+    <div className={styles['nav-left']}>
+      <a href="/">Icon logo</a>
+      <a href="/">Github</a>
+      <a href="/">Linkedin</a>
+      <a href="/">Twitter</a>
+    </div>
+    <div className={styles['nav-right']}>
       <a href="/">About</a>
       <a href="/">Projects</a>
       <a href="/">Contact</a>
       <a href="/">Resume</a>
-      </div>
-    </nav>
-  )
-}
+    </div>
+  </nav>
+);
 
 export default Navbar;
