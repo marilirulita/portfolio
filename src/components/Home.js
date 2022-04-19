248d42f1d390b31ad9c63e94d002658b6aa7fd11
mar <marilirulita@gmail.com>
2022-04-18 19:50:41 -0700

    Add list bootstrap links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,43 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup'
+import { ListGroupItem } from 'react-bootstrap';
+import '../styles/home.css';
+
 
 const Home = () => (
-  <div class="intro-header">
-  <div class="bg-overlay">
+  <div className="intro-header">
+  <div className="bg-overlay">
     <Container>
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="intro-message">
+      <Row>
+        <Col lg={12}> 
+          <div className="intro-message">
             <h1>Web Developer</h1>
             <h3>Focused in help startups take their products from concept to launch.</h3>
-            <hr class="intro-divider" />
-            <ul class="list-inline intro-social-buttons">
-              <li>
-                <a href="https://twitter.com/marylirulita" target="_blank" class="btn btn-default btn-lg"><i
-                    class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-              </li>
-              <li>
-                <a href="https://github.com/marilirulita" target="_blank" class="btn btn-default btn-lg "><i
-                    class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/marbautista/" target="_blank" class="btn btn-default btn-lg"><i
-                    class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
-              </li>
-              <li>
+            <hr className="intro-divider" />
+            <ListGroup horizontal as="ul">
+              <ListGroupItem as="li">
+                <a href="https://twitter.com/marylirulita" target="_blank" className="btn btn-default btn-lg"><i
+                    className="fa fa-twitter fa-fw"></i> <span className="network-name">Twitter</span></a>
+              </ListGroupItem>
+              <ListGroupItem as="li">
+                <a href="https://github.com/marilirulita" target="_blank" className="btn btn-default btn-lg "><i
+                    className="fa fa-github fa-fw"></i> <span className="network-name">Github</span></a>
+              </ListGroupItem>
+              <ListGroupItem as="li">
+                <a href="https://www.linkedin.com/in/marbautista/" target="_blank" className="btn btn-default btn-lg"><i
+                    className="fa fa-linkedin fa-fw"></i> <span className="network-name">Linkedin</span></a>
+              </ListGroupItem>
+              <ListGroupItem as="li">
                 <a href="https://medium.com/@marilirulita" target="_blank"
-                  class="btn btn-default btn-lg"><i class="fa fa-medium fa-fw"></i> <span
-                    class="network-name">Medium</span></a>
-              </li>
-            </ul>
+                  className="btn btn-default btn-lg"><i className="fa fa-medium fa-fw"></i> <span
+                  className="network-name">Medium</span></a>
+              </ListGroupItem>
+            </ListGroup>
           </div>
-        </div>
-        </div>
+        </Col>
+        </Row>
       </Container>
   </div>
 </div>
