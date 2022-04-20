45ba0003f840e7d570d9eeb0042211df9f47cebb
mar <marilirulita@gmail.com>
2022-04-19 17:03:47 -0700

    Add buttons icons and bootstrap in home file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,64 +1,54 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { ListGroupItem } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { FaTwitter, FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
 import '../styles/home.css';
 
 const Home = () => (
   <div className="intro-header">
     <div className="bg-overlay">
       <Container>
-        <Row>
-          <Col lg={12}>
             <div className="intro-message">
               <h1>Web Developer</h1>
               <h3>Focused in help startups take their products from concept to launch.</h3>
               <hr className="intro-divider" />
-              <ListGroup horizontal as="ul">
-                <ListGroupItem as="li">
-                  <a href="https://twitter.com/marylirulita" target="_blank" className="btn btn-default btn-lg" rel="noreferrer">
-                    <i
-                      className="fa fa-twitter fa-fw"
-                    />
+              <Row>
+                <Col sm={6} md={3} className={'mb-3'}>
+                  <Button href="https://twitter.com/marylirulita" target="_blank" size="lg" rel="noreferrer" variant="custom">
+                    <FaTwitter className="fa-fw fa-beat" />
                     {' '}
                     <span className="network-name">Twitter</span>
-                  </a>
-                </ListGroupItem>
-                <ListGroupItem as="li">
-                  <a href="https://github.com/marilirulita" target="_blank" className="btn btn-default btn-lg " rel="noreferrer">
-                    <i
-                      className="fa fa-github fa-fw"
-                    />
+                  </Button>
+                </Col>
+                <Col sm={6} md={3} className={'mb-3'}>
+                  <Button href="https://github.com/marilirulita" target="_blank" size="lg" rel="noreferrer">
+                    <FaGithub className="fa-fw" />
                     {' '}
                     <span className="network-name">Github</span>
-                  </a>
-                </ListGroupItem>
-                <ListGroupItem as="li">
-                  <a href="https://www.linkedin.com/in/marbautista/" target="_blank" className="btn btn-default btn-lg" rel="noreferrer">
-                    <i
-                      className="fa fa-linkedin fa-fw"
-                    />
+                  </Button>
+                </Col>
+                <Col sm={6} md={3} className={'mb-3'}>
+                  <Button href="https://www.linkedin.com/in/marbautista/" target="_blank" size="lg"rel="noreferrer">
+                    <FaLinkedin className="fa-fw" />
                     {' '}
                     <span className="network-name">Linkedin</span>
-                  </a>
-                </ListGroupItem>
-                <ListGroupItem as="li">
-                  <a
+                  </Button>
+                </Col>
+                <Col sm={6} md={3} className={'mb-3'}>
+                  <Button
                     href="https://medium.com/@marilirulita"
                     target="_blank"
-                    className="btn btn-default btn-lg"
+                    size="lg"
                     rel="noreferrer"
                   >
-                    <i className="fa fa-medium fa-fw" />
+                    <FaMedium className="fa-fw" />
                     {' '}
                     <span className="network-name">Medium</span>
-                  </a>
-                </ListGroupItem>
-              </ListGroup>
+                  </Button>
+                </Col>
+              </Row>
             </div>
-          </Col>
-        </Row>
       </Container>
     </div>
   </div>
