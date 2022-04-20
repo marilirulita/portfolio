8f77a7cc70f22512a9fe2e0cbae06bacfc7e310c
mar <marilirulita@gmail.com>
2022-04-19 18:09:43 -0700

    Add footer basic style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,22 @@
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup'
+
 const Footer = () => (
   <footer>
-
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <ul className="list-inline">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li className="footer-menu-divider">&sdot;</li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li className="footer-menu-divider">&sdot;</li>
-            <li>
-              <a href="#portfolio">Portfolio</a>
-            </li>
-            <li className="footer-menu-divider">&sdot;</li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+    <Container>
+          <ListGroup horizontal={'sm'}>
+            <ListGroup.Item><Link to={"/"}>Home</Link></ListGroup.Item>
+            <ListGroup.Item><Link to={"/about"}>About</Link></ListGroup.Item>
+            <ListGroup.Item><Link to={"/projects"}>Projects</Link></ListGroup.Item>
+            <ListGroup.Item><Link to={"/contact"}>Contact</Link></ListGroup.Item>
+          </ListGroup>
           <p className="copyright text-muted small">
             Copyright &copy; Mar Bautista Web Developer Freelance 2022. All Rights Reserved
           </p>
-        </div>
-      </div>
-    </div>
+    </Container>
   </footer>
 );
 
