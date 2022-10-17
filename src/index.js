e8cf4ba17d79e90b736b486f0f9bfc9dae27d0a4
mar <marilirulita@gmail.com>
2022-10-17 14:45:53 -0700

    add new portfolio design

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
-import './App.css';
-import App from './App';
+// import './App.css';
+// import App from './App';
+import Portfolio from './Portfolio';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Portfolio />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
